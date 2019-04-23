d304e80aaaa79408b8f59a76b0d3efe56a280562
thetai3110 <trthetai3110@gmail.com>
2019-04-23 22:16:34 +0700

    exam

--- src/app/admin/exam-certificate/exam/exam-form/exam-form.component.ts
+++ src/app/admin/exam-certificate/exam/exam-form/exam-form.component.ts
@@ -3,6 +3,7 @@ import { ClassesService } from 'src/app/services/classes.service';
 import { ExamService } from 'src/app/services/exam.service';
 import { MatDialogRef, MatSnackBar } from '@angular/material';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-exam-form',
@@ -11,48 +12,103 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ExamFormComponent implements OnInit {
 
-  form: FormGroup = new FormGroup({
-    idClass: new FormControl(''),
-    dayExam: new FormControl(new Date(), [Validators.required]),
-    timeExam: new FormControl('', [Validators.required]),
-    duration: new FormControl('',[Validators.required]),
-    status: new FormControl('1', [Validators.required])
-  }); 
+  // form: FormGroup = new FormGroup({
+  //   idClass: new FormControl(''),
+  //   dayExam: new FormControl(new Date(), [Validators.required]),
+  //   timeExam: new FormControl('', [Validators.required]),
+  //   duration: new FormControl('',[Validators.required]),
+  //   status: new FormControl('1', [Validators.required])
+  // }); 
 
-  public hasError = (controlName: string, errorName: string) =>{
+  form: FormGroup = new FormGroup({
+    idCourse: new FormControl,
+    duration: new FormControl,
+    status: new FormControl,
+  });
+
+
+  public hasError = (controlName: string, errorName: string) => {
     return this.form.controls[controlName].hasError(errorName);
   }
 
   constructor(private examService: ExamService,
     private classesService: ClassesService,
+    private courseService: CourseService,
     public dialogRef: MatDialogRef<ExamFormComponent>,
     private snackBar: MatSnackBar) { }
 
-  allClass : any;
+  classByCourse: any;
+  allCourse: any;
+  idCourse = "";
+  showClass = true;
+  showDate = true;
+  listClass = [];
+  classes = [];
 
   ngOnInit() {
-    this.classesService.getData().subscribe(data=>{
-      if(data != null)
-        this.allClass = data;
-    })
+    this.courseService.getData().subscribe(data => {
+      if (data != null)
+        this.allCourse = data;
+    });
   }
 
-  onCancel(){
+  onCancel() {
     this.dialogRef.close();
   }
 
-  onSubmit(form){
-    this.examService.postData(form).subscribe(data =>{
-      if(data!= null){
-        this.snackBar.open("Success!!!", "Add", {
-          duration: 2000,
-        });
-      }else{
-        this.snackBar.open("Fail!!!", "Add", {
-          duration: 2000,
-        });
+  onChangeCourse(event) {
+    if (event.target.value != "") {
+      this.loadDataByIdCourse(event.target.value);
+      this.showClass = false;
+    } else {
+      this.showClass = true;
+    }
+  }
+
+  onChoose(event, idClass) {
+    if (event.checked == true) {
+      this.loadDataByIdClass(idClass);
+      this.showDate = false;
+    } else {
+      var pos = this.listClass.indexOf(idClass);
+      this.listClass.splice(pos, 1);
+      this.form.removeControl("form-"+idClass);
+      this.form.removeControl("time-"+idClass);
+    }
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+
+    // this.examService.postData(form).subscribe(data =>{
+    //   if(data!= null){
+    //     this.snackBar.open("Success!!!", "Add", {
+    //       duration: 2000,
+    //     });
+    //   }else{
+    //     this.snackBar.open("Fail!!!", "Add", {
+    //       duration: 2000,
+    //     });
+    //   }
+    //   this.onCancel();
+    // });
+  }
+
+
+  loadDataByIdCourse(id) {
+    this.classesService.getDataByIdCourse(id).subscribe(data => {
+      if (data != null)
+        this.classByCourse = data;
+    });
+  }
+
+  loadDataByIdClass(id) {
+    this.classesService.getDataById(id).subscribe(data => {
+      if (data != null) {
+        this.listClass.push(data);
+        this.form.addControl("form-"+id, new FormControl());
+        this.form.addControl("time-"+id, new FormControl());
       }
-      this.onCancel();
     });
   }
 
